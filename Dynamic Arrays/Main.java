97efdff6d900b2ae7bf6466d2e8c70d7392d2e91
Aditya <59937191+Aditya-Tripuraneni@users.noreply.github.com>
2023-08-27 11:57:16 -0400

    Update Main.java
    added linkedlists vs array lists testings

--- Dynamic Arrays/Main.java
+++ Dynamic Arrays/Main.java
@@ -68,6 +68,85 @@ public class Main {
         System.out.println(dynamicArray.getSize());
         System.out.println(dynamicArray.getCapacity());
 
+        // LinkedLists vs Array List testings
+
+        ArrayList<Integer> myArrayList = new ArrayList<Integer>(); 
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+
+        long startTime;
+        long endTime;
+        long elapsedTime;
+
+        for (int i = 0; i < 1_000_000; i ++){
+            linkedList.add(i);
+            myArrayList.add(i);
+
+        }
+
+        startTime = System.nanoTime();
+
+
+        //linkedlist operation bellow
+        linkedList.remove(linkedList.size() -1);
+
+
+        endTime = System.nanoTime();
+
+        elapsedTime = endTime - startTime; 
+
+        System.out.println("LinkedList: \t" + elapsedTime + " ns");
+
+        startTime = System.nanoTime();
+
+        // array list operation bellow
+        myArrayList.remove(myArrayList.size() -1);
+
+        endTime = System.nanoTime();
+
+        elapsedTime = endTime - startTime; 
+
+        System.out.println("ArrayList: \t" + elapsedTime + " ns");
+
+
+
+
+        // ! to get the 0th element of linked list took approx: 26300 ns
+        // ! To get the 0th element of array list took approx:  13500ns 
+
+        //! To get the middle element of a linkedlist took approx: 7487700 ns 
+        //! To get the middle element of an array list took approx: 17200 ns 
+
+        //? We can see that the array list is a lot faster with grabbing elements
+
+        //*Conclusion:  Accessing an element from an array list is faster than accessing an element from a linked list
+
+        /*
+         * The following tests bellow are conducted with removal and insertion with the linked list and the array list
+         */
+
+        // ! to remove the 0th element of linked list took approx: 27700 ns
+        // ! To remove the 0th element of array list took approx:  547700 ns 
+
+        //! To remove the middle element of a linkedlist took approx: 5476200 ns 
+        //! To remove the middle element of an array list took approx: 309900 ns 
+
+
+        //! To remove the lsat element of a linkedlist took approx: 36000 ns 
+        //! To remove the last element of an array list took approx: 17400 ns 
+
+        /*
+         * So array lists are more flexible than linked lists.
+         * If the data set is extremly large, and we are doing a lot of insertion operations it may be better to use a linkedlist
+         * We saw that the arraylist out performed the linkedlist for deleting elements in the middle and at the end
+         * The linkedlist was faster with removing the element at the start
+         * Traversing a linkedlist takes longer since there are no indicies to refer to
+         */
+
+
+
+
+
+
 
 
 
